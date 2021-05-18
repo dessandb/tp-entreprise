dc8d301e66670c01ad495825787ec8a91dab0da2
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-18 10:26:21 +0200

    commit app et correction

--- tp-entreprise-jpa/src/main/java/sopra/entreprise/Application.java
+++ tp-entreprise-jpa/src/main/java/sopra/entreprise/Application.java
@@ -4,9 +4,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import sopra.entreprise.repository.IPersonneRepository;
+import sopra.entreprise.repository.IPropositionStageRepository;
 import sopra.entreprise.repository.jpa.PersonneRepositoryJpa;
+import sopra.entreprise.repository.jpa.PropositionStageRepositoryJpa;
+import sopra.entreprise.repository.IConventionRepository;
 import sopra.entreprise.repository.IEntrepriseRepository;
+import sopra.entreprise.repository.IEtudiantContactEntrepriseRepository;
+import sopra.entreprise.repository.IEtudiantRepository;
+import sopra.entreprise.repository.jpa.ConventionRepositoryJpa;
 import sopra.entreprise.repository.jpa.EntrepriseRepositoryJpa;
+import sopra.entreprise.repository.jpa.EtudiantContactEntrepriseRepositoryJpa;
+import sopra.entreprise.repository.jpa.EtudiantRepositoryJpa;
 
 
 
@@ -16,8 +24,13 @@ public class Application {
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("tp-entreprise-jpa");
 
 	private final IPersonneRepository personneRepo = new PersonneRepositoryJpa();
-	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("tpvol-jpa");
 	private final IEntrepriseRepository entrepriseRepo = new EntrepriseRepositoryJpa();
+	private final IConventionRepository conventionRepo = new ConventionRepositoryJpa();
+	private final IEtudiantRepository etudiantRepo = new EtudiantRepositoryJpa();
+	private final IEtudiantContactEntrepriseRepository etudiantcontactentrepriseRepo = new EtudiantContactEntrepriseRepositoryJpa();
+	private final IPropositionStageRepository propositionRepo = new PropositionStageRepositoryJpa();
+	
+	
 	
 	private Application() {
 
@@ -42,5 +55,27 @@ public class Application {
 	public IPersonneRepository getPersonneRepo() {
 		return personneRepo;
 	}
+
+	public IEntrepriseRepository getEntrepriseRepo() {
+		return entrepriseRepo;
+	}
+
+	public IConventionRepository getConventionRepo() {
+		return conventionRepo;
+	}
+
+	public IEtudiantRepository getEtudiantRepo() {
+		return etudiantRepo;
+	}
+
+	public IEtudiantContactEntrepriseRepository getEtudiantcontactentrepriseRepo() {
+		return etudiantcontactentrepriseRepo;
+	}
+
+	public IPropositionStageRepository getPropositionRepo() {
+		return propositionRepo;
+	}
+	
+	
 
 }
