076661085d7bb3716b4b1a6e673ec43a3abb13d8
Florian <flo>
2021-05-18 09:47:17 +0200

    Commit conventio/propstage

--- tp-entreprise-jpa/src/main/java/sopra/entreprise/repository/jpa/ConventionRepositoryJpa.java
+++ tp-entreprise-jpa/src/main/java/sopra/entreprise/repository/jpa/ConventionRepositoryJpa.java
@@ -1,5 +1,133 @@
 package sopra.entreprise.repository.jpa;
 
-public class ConventionRepositoryJpa {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.entreprise.model.Convention;
+import sopra.entreprise.repository.IConventionRepository;
+import sopra.entreprise.Application;
+
+public class ConventionRepositoryJpa implements IConventionRepository{
+
+	@Override
+	public List<Convention> findAll() {
+		List<Convention> conventions = new ArrayList<Convention>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Convention> query = em.createQuery("select c from Convention c", Convention.class);
+
+			conventions = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return conventions;
+	}
+
+	@Override
+	public Convention findById(Integer id) {
+		Convention convention = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			convention = em.find(Convention.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return convention;
+	}
+
+	@Override
+	public Convention save(Convention obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	@Override
+	public void delete(Convention obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		
+	}
 
 }
---
+++ tp-entreprise-jpa/src/main/java/sopra/entreprise/repository/jpa/PropositionStageRepositoryJpa.java
@@ -0,0 +1,133 @@
+package sopra.entreprise.repository.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.entreprise.repository.IPropositionStageRepository;
+import sopra.entreprise.Application;
+import sopra.entreprise.model.PropositionStage;
+
+public class PropositionStageRepositoryJpa implements IPropositionStageRepository{
+
+	@Override
+	public List<PropositionStage> findAll() {
+		List<PropositionStage> propositionStages = new ArrayList<PropositionStage>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<PropositionStage> query = em.createQuery("select p from PropositionStage p", PropositionStage.class);
+
+			propositionStages = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return propositionStages;
+	}
+
+	@Override
+	public PropositionStage findById(Integer id) {
+		PropositionStage propositionStage = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			propositionStage = em.find(PropositionStage.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return propositionStage;
+	}
+
+	@Override
+	public PropositionStage save(PropositionStage obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	@Override
+	public void delete(PropositionStage obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		
+	}
+
+}
